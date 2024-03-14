2148f7387c9d5ae784b882d33512d9edfa0ff640
Tony Russo <ynotrusso@users.noreply.github.com>
2024-03-14 11:49:49 +0700

    add fruits get and post routes

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 import express from 'express'
+import 'dotenv/config'
 const app = express()
+import db from './db.js'
+
+app.get('/fruits', async (req, res) => {
+    let price = req.query.price
+    
+    let myQuery = `SELECT * FROM fruits`
+
+    if (req.query.price) {
+        myQuery += ` WHERE price = ${price}`
+    }
+    
+    const response = await db.query(myQuery)
+    res.json(response.rows)
+})
+
+app.post('/fruits', (req, res) => {
+    db.query(`INSERT INTO fruits (name, price, featured)
+    VALUES ('mango', 15, true)`)
+    res.send('fruit inserted')
+})
 
 app.listen(4010, () => {
     console.log('Server is running on port 4010')
